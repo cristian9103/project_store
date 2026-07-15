636f874300261f34079f860234766ffe0aabde9c
Cristian <cristiankmilo1103@gmail.com>
2026-07-15 18:03:51 -0500

    Se agrega el modelo Pedido a la app pedidos

---
+++ pedidos/models/pedidos.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+class EstadoPedido(models.TextChoices):
+    PENDIENTE = "PE", "Pendiente"
+    PAGADO = "PA", "Pagado"
+    PREPARACION = "PR", "En preparación"
+    ENVIADO = "EN", "Enviado"
+    ENTREGADO = "ET", "Entregado"
+    CANCELADO = "CA", "Cancelado"
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(
+        "clientes.Cliente",
+        on_delete=models.PROTECT,
+        related_name="pedidos"
+    )
+    fecha = models.DateTimeField(auto_now_add=True)
+    
+    estado = models.CharField(
+        max_length=2,
+        choices=EstadoPedido.choices,
+        default=EstadoPedido.PENDIENTE
+    )
+    
+    subtotal = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    
+    costo_envio = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    
+    descuento = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    
+    total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    
+    class Meta:
+        verbose_name = "Pedido"
+        verbose_name_plural = "Pedidos"
+        ordering = ["-fecha"]
+        db_table = "pedidos"
+        
+    def __str__(self):
+        return f"Pedido # {self.pk} - {self.cliente}"
