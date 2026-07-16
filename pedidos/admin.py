25b9909b2dc1d4c068dc285930b53bd87214465a
Cristian <cristiankmilo1103@gmail.com>
2026-07-15 20:36:37 -0500

    Se agregan los modelos Pedido y DetallePedido al admin de pedidos

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from pedidos.models.detalles_pedido import DetallePedido
+from pedidos.models.pedidos import Pedido
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = (
+        "cliente",
+        "fecha",
+        "estado",
+        "subtotal",
+        "costo_envio",
+        "descuento",
+        "total",
+    )
+    
+    search_fields = (
+        "fecha",
+        "estado",
+    )
+    
+    list_filter = (
+        "fecha",
+        "estado",
+    )
+    
+    ordering = ("fecha",)
+    
+    list_select_related = ("cliente",)
+    
+@admin.register(DetallePedido)
+class DetallePedidoAdmin(admin.ModelAdmin):
+    list_display = (
+        "pedido",
+        "producto",
+        "precio_unitario",
+        "cantidad",
+        "subtotal",
+        "fecha_creacion",
+    )
+    
+    search_fields = (
+        "pedido__pk",
+        "producto__sku",
+        "fecha_creacion",
+    )
+    
+    list_filter = (
+        "pedido",
+        "producto",
+        "fecha_creacion",
+    )
+    
+    ordering = ("fecha_creacion",)
+    
+    list_select_related = ("pedido", "producto",)
