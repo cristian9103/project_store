2ad7828a1fb451758d4695b846c7ccf8f3ca464a
Cristian <cristiankmilo1103@gmail.com>
2026-07-18 18:37:28 -0500

    Se agrega el método test_calcular_subtotal_con_varios_productos al archivo test_calculos.py de la carpeta tests en la app pedidos

--- pedidos/tests/test_calculos.py
+++ pedidos/tests/test_calculos.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 
 from pedidos.models import DetallePedido
 from pedidos.services.calculos import calcular_subtotal
+from catalogo.models import Producto
 
 from .base import BaseTestCase
 
@@ -42,5 +43,34 @@ class CalculosTestCase(BaseTestCase):
         self.assertEqual(
             subtotal,
             Decimal("40_000.00")
+        )
+        
+    def test_calcular_subtotal_con_varios_productos(self):
+        
+        # Arrange
+        producto2 = Producto.objects.create(
+            categoria=self.categoria,
+            marca=self.marca,
+            sku="CER001",
+            nombre="Crema",
+            precio_compra=Decimal("25_000"),
+            precio_venta=Decimal("30_000"),
+            stock=20,
+        )
+        
+        # Act
+        self.crear_detalle(cantidad=2)
+        
+        self.crear_detalle(
+            producto=producto2,
+            cantidad=1
+        )
+        
+        subtotal = calcular_subtotal(self.pedido)
+        
+        # Assert
+        self.assertEqual(
+            subtotal,
+            Decimal("70_000.00")
         )
         
