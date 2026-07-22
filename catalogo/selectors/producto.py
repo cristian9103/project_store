2eaeccdfa2b72ef1093e851e18b10e4569f01571
Cristian <cristiankmilo1103@gmail.com>
2026-07-21 22:40:27 -0500

    Se agrega la función buscar_productos al archivo producto.py ubicado en la carpeta selectors de la app catalogo

---
+++ catalogo/selectors/producto.py
@@ -0,0 +1,31 @@
+from catalogo.models import Producto
+
+def buscar_productos(
+    *,
+    texto=None,
+    categoria=None,
+    marca=None
+):
+    queryset = Producto.objects.disponibles()
+    
+    if texto:
+        queryset = queryset.filter(
+            nombre__incontains=texto.strip()
+        )
+        
+    if categoria:
+        queryset = queryset.filter(
+            categoria_id=categoria
+        )
+        
+    if marca:
+        queryset = queryset.filter(
+            marca_id=marca
+        )
+        
+    return (
+        queryset
+        .select_related("categoria", "marca")
+        .order_by("nombre")
+    )
+    
--- catalogo/views/producto.py
+++ catalogo/views/producto.py
@@ -1,15 +1,32 @@
 from django.views.generic import ListView
 
 from catalogo.models import Producto
+from catalogo.selectors import buscar_productos
 
 class ProductoListView(ListView):
     
     model = Producto
-    
-    queryset = Producto.objects.disponibles()
     
     template_name = "catalogo/lista_productos.html"
     
     context_object_name = "productos"
     
     paginate_by = 12
+    
+    def get_queryset(self):
+        params = self.request.GET
+        
+        return buscar_productos(
+            texto=params.get("buscar"),
+            categoria=params.get("categoria"),
+            marca=params.get("marca"),
+        )
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context["buscar"] = self.request.GET.get("buscar", "")
+        context["categoria"] = self.request.GET.get("categoria", "")
+        context["marca"] = self.request.GET.get("marca", "")
+        
+        return context
