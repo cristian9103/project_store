23fe99844ee720aa55a4840594841d1cb668945b
Cristian <cristiankmilo1103@gmail.com>
2026-07-22 09:37:17 -0500

    Se agrega la clase ProductoBusquedaForm al archivo producto.py ubicado en la carpeta forms de la app catalogo. Se modifica el archivo producto.py ubicado en la carpeta views de la app catalogo

---
+++ catalogo/forms/producto.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from catalogo.models import Categoria, Marca
+
+class ProductoBusquedaForm(forms.Form):
+    buscar = forms.CharField(
+        required=False,
+        max_length=100,
+        label="Buscar",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Buscar productos...",
+            }
+        ),
+    )
+    
+    categoria = forms.ModelChoiceField(
+        queryset=Marca.objects.all(),
+        required=False,
+        empty_label="Todas las marcas",
+    )
+    
--- catalogo/views/producto.py
+++ catalogo/views/producto.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView
 
 from catalogo.models import Producto
 from catalogo.selectors import buscar_productos
+from catalogo.forms import ProductoBusquedaForm
 
 class ProductoListView(ListView):
     
@@ -14,19 +15,20 @@ class ProductoListView(ListView):
     paginate_by = 12
     
     def get_queryset(self):
-        params = self.request.GET
+        self.form = ProductoBusquedaForm(self.request.GET)
         
-        return buscar_productos(
-            texto=params.get("buscar"),
-            categoria=params.get("categoria"),
-            marca=params.get("marca"),
-        )
+        if self.form.is_valid():
+            return buscar_productos(
+                texto=self.form.cleaned_data["buscar"],
+                categoria=self.form.cleaned_data["categoria"],
+                marca=self.form.cleaned_data["marca"],
+            )
+            
+        return Producto.objects.disponibles()
         
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        context["buscar"] = self.request.GET.get("buscar", "")
-        context["categoria"] = self.request.GET.get("categoria", "")
-        context["marca"] = self.request.GET.get("marca", "")
+        context["form"] = self.form
         
         return context
