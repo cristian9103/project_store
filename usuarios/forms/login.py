8b672e8172ae0119a836d553c1d7be9b8958ff77
Cristian <cristiankmilo1103@gmail.com>
2026-07-27 19:57:53 -0500

    Se agrega el archivo templates/usuarios/login.html y se modifica el archivo usuarios/forms/login.py

--- usuarios/forms/login.py
+++ usuarios/forms/login.py
@@ -1,4 +1,18 @@
 from django.contrib.auth.forms import AuthenticationForm
 
 class LoginForm(AuthenticationForm):
-    pass
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields["username"].label = "Correo electrónico"
+        
+        self.fields["username"].widget.attrs.update({
+            "placeholder": "correo@ejemplo.com",
+            "autocomplete": "email",
+        })
+        
+        self.fields["password"].widget.attrs.update({
+            "placeholder": "Contraseña",
+            "autocomplete": "current-password",
+        })
