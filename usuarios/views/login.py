c0f28f02076bf15ab0195143c19d5946a2b3937e
Cristian <cristiankmilo1103@gmail.com>
2026-07-28 23:20:28 -0500

    Se modifica el archivo usuarios/views/login.py

--- usuarios/views/login.py
+++ usuarios/views/login.py
@@ -6,3 +6,6 @@ class UsuarioLoginView(LoginView):
     template_name = "usuarios/login.html"
     authentication_form = LoginForm
     redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return self.get_default_redirect_url()
