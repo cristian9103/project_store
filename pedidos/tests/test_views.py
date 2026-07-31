c41692a51326caee3419fcacb98fa3bc083a5019
Cristian <cristiankmilo1103@gmail.com>
2026-07-31 17:53:27 -0500

    Se agrega la clase CarritoDetailViewTest al archivo pedidos/tests/test_views.py

---
+++ pedidos/tests/test_views.py
@@ -0,0 +1,75 @@
+from django.urls import reverse
+from django.contrib.messages import get_messages
+
+from .base import BaseTestCase
+from clientes.models import Cliente
+from usuarios.models import Usuario
+from pedidos.models import Pedido, EstadoPedido
+from pedidos.services import ZERO
+
+class CarritoDetailViewTest(BaseTestCase):
+    
+    def test_get_muestra_carrito(self):
+        
+        self.client.force_login(self.usuario)
+        
+        url = reverse("pedidos:carrito")
+        
+        response = self.client.get(url)
+        
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+        
+    def test_carrito_corresponde_al_usuario_autenticado(self):
+        
+        otro_usuario = Usuario.objects.create_user(
+            email="otro@test.com",
+            password="123456789",
+            first_name="Otro",
+            last_name="Usuario",
+        )
+        
+        otro_cliente = Cliente.objects.create(
+            usuario=otro_usuario,
+            documento="987654321",
+            telefono="3119876543",
+        )
+        
+        otro_pedido = Pedido.objects.create(
+            cliente=otro_cliente,
+            estado=EstadoPedido.PENDIENTE,
+            subtotal=ZERO,
+            costo_envio=ZERO,
+            total=ZERO,
+        )
+        
+        self.client.force_login(self.usuario)
+        
+        response = self.client.get(
+            reverse("pedidos:carrito")
+        )
+        
+        self.assertEqual(
+            response.context["pedido"],
+            self.pedido
+        )
+        
+        self.assertNotEqual(
+            response.context["pedido"],
+            otro_pedido
+        )
+        
+    def test_carrito_requiere_autenticacion(self):
+        
+        carrito_url = reverse("pedidos:carrito")
+        
+        login_url = reverse("usuarios:login")
+        
+        response = self.client.get(carrito_url)
+        
+        self.assertRedirects(
+            response,
+            f"{login_url}?next={carrito_url}"
+        )
