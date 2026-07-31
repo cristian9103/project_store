741a6e5f6682e86c7ad39677f5df1049adc25bfe
Cristian <cristiankmilo1103@gmail.com>
2026-07-31 07:47:20 -0500

    Se agrega la clase BaseTestCase al archivo usuarios/tests/base.py

---
+++ usuarios/tests/base.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+
+from usuarios.models import Usuario
+from clientes.models import Cliente
+
+class BaseTestCase(TestCase):
+    
+    def crear_usuario(
+        self,
+        email="usuario@test.com",
+        password="TestPassword123!",
+        first_name="Cristian",
+        last_name="Ramirez",
+    ):
+        usuario = Usuario.objects.create_user(
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        
+        return usuario
+    
+    def crear_cliente(
+        self,
+        usuario=None,
+        documento="123456789",
+        telefono="3001234567",
+    ):
+        if usuario is None:
+            usuario = self.crear_usuario()
+            
+        cliente = Cliente.objects.create(
+            usuario=usuario,
+            documento=documento,
+            telefono=telefono,
+        )
+        
+        return cliente
