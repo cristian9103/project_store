c0cf540be1e7f3e97a259e94546af5337e988dae
Cristian <cristiankmilo1103@gmail.com>
2026-08-03 20:55:14 -0500

    Se agregan los métodos test_detalle_no_permite_cantidad_cero, test_detalle_no_permite_precio_unitario_netativo y test_detalle_calcula_subtotal_automaticamente al archivo pedidos/tests/test_modelos.py

--- pedidos/tests/test_modelos.py
+++ pedidos/tests/test_modelos.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from django.db import IntegrityError, transaction
 
 from .base import BaseTestCase
+from pedidos.models import DetallePedido
 
 class ModelosTestCase(BaseTestCase):
     
@@ -33,3 +34,39 @@ class ModelosTestCase(BaseTestCase):
             with transaction.atomic():
                 self.pedido.descuento = Decimal("-1.00")
                 self.pedido.save()
+                
+    def test_detalle_no_permite_cantidad_cero(self):
+        
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                DetallePedido.objects.create(
+                    pedido=self.pedido,
+                    producto=self.producto,
+                    precio_unitario=Decimal("20_000.00"),
+                    cantidad=0,
+                )
+                
+    def test_detalle_no_permite_precio_unitario_negativo(self):
+        
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                DetallePedido.objects.create(
+                    pedido=self.pedido,
+                    producto=self.producto,
+                    precio_unitario=Decimal("-1.00"),
+                    cantidad=1
+                )
+                
+    def test_detalle_calcula_subtotal_automaticamente(self):
+      
+        detalle = DetallePedido.objects.create(
+            pedido=self.pedido,
+            producto=self.producto,
+            precio_unitario=Decimal("20_000.00"),
+            cantidad=3,
+        )
+        
+        self.assertEqual(
+            detalle.subtotal,
+            Decimal("60_000.00")
+        )
