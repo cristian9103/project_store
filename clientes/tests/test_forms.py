af4e85538c5ffb9d38f7f1a85a21640ab9af897b
Cristian <cristiankmilo1103@gmail.com>
2026-08-08 17:25:40 -0500

    Se agregan los métodos test_formulario_direccion_requiere_campos_obligatorios() y test_formulario_direccion_codigo_postal_es_opcional() al archivo clientes/tests/test_forms.py

--- clientes/tests/test_forms.py
+++ clientes/tests/test_forms.py
@@ -20,3 +20,29 @@ class DireccionFormTestCase(BaseTestCase):
         self.assertTrue(
             form.is_valid()
         )
+        
+    def test_formulario_direccion_requiere_campos_obligatorios(self):
+        
+        form = DireccionForm(data={})
+        
+        self.assertFalse(form.is_valid())
+        
+        self.assertIn("nombre", form.errors)
+        self.assertIn("direccion", form.errors)
+        self.assertIn("ciudad", form.errors)
+        self.assertIn("departamento", form.errors)
+        
+    def test_formulario_direccion_codigo_postal_es_opcional(self):
+        
+        form = DireccionForm(
+            data={
+                "nombre": "Casa",
+                "direccion": "Carrera 10 # 20-30",
+                "ciudad": "Medellín",
+                "departamento": "Antioquia",
+                "codigo_postal": "",
+                "es_principal": False,
+            }
+        )
+        
+        self.assertTrue(form.is_valid())
