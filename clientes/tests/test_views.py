6d67a28476be88754909b83123248a3b49c46f80
Cristian <cristiankmilo1103@gmail.com>
2026-08-08 20:43:33 -0500

    Se agregan los métodos test_crear_direccion_se_asocia_al_cliente_autenticado() y test_listar_direcciones_muestra_las_del_cliente() al archivo cliente/tests/test_views.py

--- clientes/tests/test_views.py
+++ clientes/tests/test_views.py
@@ -93,3 +93,76 @@ class DireccionViewTestCase(BaseTestCase):
                 cliente=self.cliente
             ).exists()
         )
+        
+    def test_crear_direccion_se_asocia_al_cliente_autenticado(self):
+        
+        self.client.force_login(self.usuario)
+        
+        response = self.client.post(
+            reverse("clientes:crear_direccion"),
+            data={
+                "nombre": "Trabajo",
+                "direccion": "Carrera 50 # 10-20",
+                "ciudad": "Medellín",
+                "departamento": "Antioquia",
+                "codigo_postal": "050002",
+                "es_principal": False,
+            },
+        )
+        
+        self.assertEqual(
+            response.status_code,
+            302,
+        )
+        
+        direccion = Direccion.objects.get(
+            nombre="Trabajo"
+        )
+        
+        self.assertEqual(
+            direccion.cliente,
+            self.cliente
+        )
+        
+        self.assertEqual(
+            direccion.cliente.usuario,
+            self.usuario
+        )
+        
+    def test_listar_direcciones_muestra_las_del_cliente(self):
+        
+        self.client.force_login(self.usuario)
+        
+        Direccion.objects.create(
+            cliente=self.cliente,
+            nombre="Casa",
+            direccion="Carrera 10 # 20-30",
+            ciudad="Medellín",
+            departamento="Antioquia",
+            codigo_postal="050001",
+            es_principal=True,
+        )
+        
+        Direccion.objects.create(
+            cliente=self.cliente,
+            nombre="Trabajo",
+            direccion="Carrera 50 # 10-20",
+            ciudad="Medellín",
+            departamento="Antioquia",
+            codigo_postal="050002",
+            es_principal=False,
+        )
+        
+        response = self.client.get(
+            reverse("clientes:lista_direcciones")
+        )
+        
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+        
+        self.assertEqual(
+            len(response.context["direcciones"]),
+            2,
+        )
--- clientes/urls.py
+++ clientes/urls.py
@@ -6,4 +6,5 @@ app_name = "clientes"
 
 urlpatterns = [
     path("direcciones/nueva/", views.crear_direccion, name="crear_direccion",),
+    path("direcciones/", views.lista_direcciones, name="lista_direcciones",),
 ]
