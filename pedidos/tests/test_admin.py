764fb730afd18f30caf99cd6fdbda4aa9ceda410
Cristian <cristiankmilo1103@gmail.com>
2026-08-27 16:18:34 -0500

    Se agregan los métodos test_enviar_pedidos_seleccionados_con_un_pedido_invalido_no_cambia_ninguno() y test_enviar_pedidos_esta_registrada_como_accion() al archivo pedidos/tests/test_admin.py

--- pedidos/tests/test_admin.py
+++ pedidos/tests/test_admin.py
@@ -65,3 +65,79 @@ class PedidoAdminTestCase(BaseTestCase):
             pedido_2.estado,
             EstadoPedido.ENVIADO,
         )
+        
+    def test_enviar_pedidos_seleccionados_con_un_pedido_invalido_no_cambia_ninguno(self):
+        self.pedido.estado = EstadoPedido.PREPARACION
+        self.pedido.save(update_fields=["estado"])
+        
+        cliente_2 = Cliente.objects.create(
+            usuario=self.otro_usuario,
+            documento="987654321",
+            telefono="3119876543",
+        )
+        
+        pedido_2 = Pedido.objects.create(
+            cliente=cliente_2,
+            estado=EstadoPedido.PENDIENTE,
+            subtotal=ZERO,
+            costo_envio=ZERO,
+            descuento=ZERO,
+            total=ZERO,
+        )
+        
+        queryset = Pedido.objects.filter(
+            pk__in=[
+                self.pedido.pk,
+                pedido_2.pk,
+            ]
+        )
+        
+        request = RequestFactory().get("/admin/pedidos/pedido/")
+        
+        pedido_admin = PedidoAdmin(
+            Pedido,
+            admin.site,
+        )
+        
+        with patch.object(
+            pedido_admin,
+            "message_user",
+        ):   
+            pedido_admin.enviar_pedidos(
+                request,
+                queryset,
+            )
+        
+        self.pedido.refresh_from_db()
+        pedido_2.refresh_from_db()
+        
+        self.assertEqual(
+            self.pedido.estado,
+            EstadoPedido.PREPARACION,
+        )
+        
+        self.assertEqual(
+            pedido_2.estado,
+            EstadoPedido.PENDIENTE,
+        )
+        
+    def test_enviar_pedidos_esta_registrada_como_accion(self):
+        request = RequestFactory().get(
+            "/admin/pedidos/pedido"
+        )
+        
+        request.user = self.usuario
+        
+        pedido_admin = PedidoAdmin(
+            Pedido,
+            admin.site,
+        )
+        
+        acciones = dict(
+            pedido_admin.get_actions(request)
+        )
+        
+        self.assertIn(
+            "enviar_pedidos",
+            acciones,
+        )
