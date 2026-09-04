af6332299c634112d6fc4b1d87ade8c77dd570a3
Cristian <cristiankmilo1103@gmail.com>
2026-09-03 20:29:57 -0500

    Se modifica el archivo pedidos/views/detalle.py. Y se agrega el archivo templates/pedidos/detalle/detalle.html

--- pedidos/views/detalle.py
+++ pedidos/views/detalle.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.views import View
+from django.views.generic import DetailView
 
-class DetallePedidoView(LoginRequiredMixin, View):
+from clientes.selectors import obtener_cliente
+from pedidos.models import Pedido
+
+class DetallePedidoView(LoginRequiredMixin, DetailView):
+    model = Pedido
+    template_name = "pedidos/detalle/detalle.html"
+    context_object_name = "pedido"
     
-    def get(self, request, pk):
-        return HttpResponse("Detalle del pedido")
+    def get_queryset(self):
+        cliente = obtener_cliente(self.request.user)
+        
+        return Pedido.objects.filter(
+            cliente=cliente
+        )
     
 detalle = DetallePedidoView.as_view()
