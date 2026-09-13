e1b0abcce3a8916dd17a904fcd6bff1aa9d609f1
Cristian <cristiankmilo1103@gmail.com>
2026-09-13 15:51:24 -0500

    Se agregan los métodos test_detalle_producto_agregar_al_carrito_actualiza_totales() y test_detalle_producto_agregar_al_carrito_producto_existente_suma_cantidad() al archivo catalogo/tests/test_views.py

--- catalogo/tests/test_views.py
+++ catalogo/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse
 
+from decimal import Decimal
+
 from core.tests import BaseTestCase
 
 class ProductoDetailViewTest(BaseTestCase):
@@ -73,4 +75,83 @@ class ProductoDetailViewTest(BaseTestCase):
         self.assertEqual(
             detalle.cantidad,
             2,
+        )
+        
+    def test_detalle_producto_agregar_al_carrito_actualiza_totales(self):
+        self.client.force_login(self.usuario)
+        
+        response = self.client.post(
+            reverse(
+                "pedidos:agregar_producto",
+                kwargs={"pk": self.producto.pk},
+            ),
+            data={
+                "cantidad": 2,
+            },
+        )
+        
+        self.assertEqual(
+            response.status_code,
+            302,
+        )
+        
+        self.pedido.refresh_from_db()
+        
+        self.assertEqual(
+            self.pedido.subtotal,
+            Decimal("40_000"),
+        )
+        
+        self.assertEqual(
+            self.pedido.total,
+            Decimal("40_000"),
+        )
+        
+    def test_detalle_producto_agregar_al_carrito_producto_existente_suma_cantidad(self):
+        self.client.force_login(self.usuario)
+        
+        self.client.post(
+            reverse(
+                "pedidos:agregar_producto",
+                kwargs={"pk": self.producto.pk},
+            ),
+            data={
+                "cantidad": 2,
+            },
+        )
+        
+        response = self.client.post(
+            reverse(
+                "pedidos:agregar_producto",
+                kwargs={"pk": self.producto.pk},
+            ),
+            data={
+                "cantidad": 3,
+            },
+        )
+        
+        self.assertEqual(
+            response.status_code,
+            302,
+        )
+        
+        self.pedido.refresh_from_db()
+        
+        detalle = self.pedido.detalles_pedido.get(
+            producto=self.producto
+        )
+        
+        self.assertEqual(
+            detalle.cantidad,
+            5,
+        )
+        
+        self.assertEqual(
+            self.pedido.subtotal,
+            Decimal("100_000"),
+        )
+        
+        self.assertEqual(
+            self.pedido.total,
+            Decimal("100_000"),
         )
